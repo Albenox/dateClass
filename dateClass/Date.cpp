51e6a3be713c37e61d0e8975822ea57fe09bc03e
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2026-04-27 00:08:07 -0400

    Added accessors and overloaded functions to break up the code a bit in the Date.cpp, aswell as declarations in Date.h

--- dateClass/Date.cpp
+++ dateClass/Date.cpp
@@ -10,31 +10,61 @@ Date::Date(int m, int d, int y) {
     setDate(m, d, y);
 }
 
+// Checks if the object's own year is a leap year
+bool Date::isLeapYear() const {
+    return isLeapYear(this->year);
+}
+
+// Checks if the year passed into the function is a leap year
+bool Date::isLeapYear(int y) const {
+    return (y % 400 == 0 || ((y % 4 == 0) && (y % 100 != 0)));
+}
+
+// Returns the last valid day for the object's own month/year
+int Date::lastDay() const {
+    return lastDay(this->month, this->year);
+}
+
+// Returns the last valid day for the month/year passed in
+int Date::lastDay(int m, int y) const {
+    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+        return 31;
+    }
+    else if (m == 4 || m == 6 || m == 9 || m == 11) {
+        return 30;
+    }
+    else if (isLeapYear(y)) {
+        return 29;
+    }
+    else {
+        return 28;
+    }
+}
+
+// Accessors
+int Date::getMonth() const {
+    return month;
+}
+
+int Date::getDay() const {
+    return day;
+}
+
+int Date::getYear() const {
+    return year;
+}
+
 // The function that will validate the inputs such as month, day, and year,
 // as well as have the leap year and last day of the month logic
 void Date::setDate(int m, int d, int y) {
     
+    // Variables
     // A bool that will determine if any of the inputs are invalid, and if so, turn to 1 and set the date to 1/1/1900
     bool invalid = 0;
     // An int that will determine the max day according to the month and potentially, leap year
-    int maxDay;
+    int maxDay = lastDay(m, y);
     
-    // A series of logic that will determine the maximum amount of days a month has to be valid
-    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
-        maxDay = 31;
-    }
-    else if (m == 4 || m == 6 || m == 9 || m == 11) {
-        maxDay = 30;
-    }
-    else if (y % 400 == 0 || ((y % 4 == 0) && (y % 100 != 0))) {
-        // For leap years in February
-        maxDay = 29;
-    }
-    else {
-        // For non leap years in February
-        maxDay = 28;
-    }
-    
+    // Setting variables
     // Logic to check if the month entered is within 1 and 12
     if (m >= 1 && m <= 12) {
         this->month = m;
@@ -51,10 +81,10 @@ void Date::setDate(int m, int d, int y) {
         invalid = 1;
     }
 
-
     // No logic required for year as there is such a thing as BCE, or negative years
     this->year = y;
 
+    // Invalid 
     // Checks to see if any of the information was invalid, which if it was, will default to 1/1/1900
     if (invalid == 1) {
         this->month = 1;
--- dateClass/Date.h
+++ dateClass/Date.h
@@ -16,8 +16,25 @@ private:
 
 //
 public:
+    
+    // Constructor that sets the initial values to month, day, and year
     Date(int m = 1, int d = 1, int y = 1900);
+    
+    // Mutator function that will validate the month, day, and year
     void setDate(int m, int d, int y);
+    
+    // Accessors that are functions which will grab the inputs and populate the private variables, with const to ensure the variables are not changed at all
+    int getMonth() const;
+    int getDay() const;
+    int getYear() const;
+
+    // Function and overloaded function that tracks if the year is a leap year
+    bool isLeapYear() const;
+    bool isLeapYear(int y) const;
+
+    // Function and overloaded function that holds the last day of the month
+    int lastDay() const;
+    int lastDay(int m, int y) const;
 };
 
 // The portion that will not be defined again if it already is, which should be the entire code
